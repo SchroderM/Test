ffbc649546d97facd639ec9f48d411c4cf9c34ca
Max <Maxa.schroder@gmail.com>
2022-04-26 09:30:58 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Home(),
+));
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello World!'),
+        centerTitle: true,
+        backgroundColor: Colors.green[600],
+      ),
+      body: Center(
+        child: Image.network('https://images.unsplash.com/photo-1421790500381-fc9b5996f343?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('PUSH'),
+        backgroundColor: Colors.green[600],
+      ),
+    );
+  }
+}
+
